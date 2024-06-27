32f4a0a1b1b932f46e21d889eb17ed113c060f0e
Ramunas <514899+ramunsk@users.noreply.github.com>
2024-06-27 12:34:22 +0300

    Command line args

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,13 @@ import { Parser } from './parser';
 import { AstVisualizer, ExpressionEvaluator, ExpressionFormatter } from './visitor';
 
 const args = process.argv;
+args.shift();
+args.shift();
 
-const input = args[2];
+const shouldFormat = args.includes('--format');
+const shouldPrintAst = args.includes('--ast');
+
+const input = args.find((a) => !a.startsWith('--'));
 if (!input) {
     console.log('Input was not provided!');
     process.exit(1);
@@ -16,16 +21,24 @@ if (error) {
     process.exit(1);
 }
 
-try {
-    const ast = new Parser(tokens!).parse();
-    const astString = new AstVisualizer(ast).visualize();
-    console.log(astString);
+let ast;
 
-    const pretty = new ExpressionFormatter(ast).format();
-    console.log(pretty);
-    const result = new ExpressionEvaluator(ast).evaluate();
-    console.log(result);
+try {
+    ast = new Parser(tokens!).parse();
 } catch (error) {
     console.log(error);
     process.exit(1);
 }
+
+if (shouldFormat) {
+    const pretty = new ExpressionFormatter(ast).format();
+    console.log(pretty);
+}
+
+if (shouldPrintAst) {
+    const astString = new AstVisualizer(ast).visualize();
+    console.log(astString);
+}
+
+const result = new ExpressionEvaluator(ast).evaluate();
+console.log(result);
